f9472ee1e177b4c07ecb67dcec4c24ba255fe05a
Donna <575708@student.inholland.nl>
2018-11-23 11:42:38 +0100

    Revert "test2.0"
    This reverts commit 76433d9b1bffc051ea872a49456078f951ec71a2.

--- Haarlem Festival/Haarlem Festival/Models/Domain Models/Jazz/Test.cs
+++ Haarlem Festival/Haarlem Festival/Models/Domain Models/Jazz/Test.cs
@@ -7,8 +7,5 @@ namespace Haarlem_Festival.Models.Domain_Models.Jazz
 {
     public class Test
     {
-        public string hallo { get; set; }
-        public int MyProperty { get; set; }
-        public string fuck { get; set; }
     }
 }
