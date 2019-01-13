5f2db221cd9624c7cee25514e120059b55ad40d8
Jesse van Duijne <jessevanduijne@live.nl>
2019-01-13 21:07:51 +0100

    Revert "Revert because of threads exception"
    This reverts commit 51a61339834088345a10fbd3e9bd013180fa20c3.

--- Haarlem Festival/Haarlem Festival/Controllers/FoodController.cs
+++ Haarlem Festival/Haarlem Festival/Controllers/FoodController.cs
@@ -7,6 +7,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using System.Net;
+using Haarlem_Festival.Data;
+using Haarlem_Festival.Data.Food;
 
 namespace Haarlem_Festival.Controllers
 {
@@ -14,18 +17,25 @@ namespace Haarlem_Festival.Controllers
     {
         IFoodRepository foodRepository = new FoodRepository();
         IEventRepository eventRepository = new EventRepository();
+        GooglePlacesApiHandler googlePlacesApiHandler = new GooglePlacesApiHandler();
 
         [HttpGet]
         public ActionResult Index()
         {
             IEnumerable<Restaurant> restaurants = foodRepository.GetAllRestaurants();
+            List<RestaurantView> restaurantViews = new List<RestaurantView>();
 
             foreach (Restaurant restaurant in restaurants)
             {
+                // Add cuisines to restaurant
                 restaurant.Cuisines = foodRepository.GetAllCuisinesForRestaurant(restaurant.RestaurantID);
+
+                // Fill list of viewmodels (reviews aren't stored in db so vm's are necessary)
+                //Review review = googlePlacesApiHandler.GetRestaurantReview(restaurant.GooglePlacesID);
+                //restaurantViews.Add(new RestaurantView(restaurant, review));
             }
 
-            return View(restaurants);
+            return View(restaurantViews);
         }
 
         public ActionResult BookRestaurant(int restaurantId)
@@ -33,6 +43,7 @@ namespace Haarlem_Festival.Controllers
             // Create Viewmodel
             RestaurantBooking booking = new RestaurantBooking();
             booking.Events = foodRepository.GetAllFoodEvents(restaurantId);
+
             booking.RestaurantName = foodRepository.GetRestaurant(restaurantId).RestaurantName;
 
             // Fix the datetime issue:
@@ -51,28 +62,37 @@ namespace Haarlem_Festival.Controllers
             // The 'booking'-parameter is filled up only with data that is being entered in the form: ticketamount (child/adult), selected event, special request & hidden restaurantID-field
             // Only a field can be passed hidden, not an entire object. That's why we pass only the restaurantId to the viewmodel
 
-            List<Ticket> tickets = new List<Ticket>();
-            Ticket ticket = new Ticket();
             Restaurant restaurant = foodRepository.GetRestaurant(booking.RestaurantId);
 
-            ticket.Amount = booking.AdultTickets + booking.ChildTickets;
-            ticket.EventId = booking.SelectedEvent;
-            ticket.Event = eventRepository.GetEvent(ticket.EventId);
-            ticket.Price = (booking.AdultTickets * restaurant.PriceAdults) + (booking.ChildTickets * restaurant.PriceChildren);            
-
-            // Create session if it doesn't exist or add ticket to existing session
-            if (Session["currentTickets"] == null)
+            if (ModelState.IsValid)
             {
-                tickets.Add(ticket);
-                Session["CurrentTickets"] = tickets;
-            }
-            else
-            {
-                List<Ticket> sessionTickets = (List<Ticket>)Session["currentTickets"];
-                sessionTickets.Add(ticket);
+                List<Ticket> tickets = new List<Ticket>();
+                Ticket ticket = new Ticket();
+
+                ticket.Amount = booking.AdultTickets + booking.ChildTickets;
+                ticket.EventId = booking.SelectedEvent;
+                ticket.Event = eventRepository.GetEvent(ticket.EventId);
+                ticket.Price = (booking.AdultTickets * restaurant.PriceAdults) + (booking.ChildTickets * restaurant.PriceChildren);
+
+                // Create session if it doesn't exist or add ticket to existing session
+                if (Session["currentTickets"] == null)
+                {
+                    tickets.Add(ticket);
+                    Session["CurrentTickets"] = tickets;
+                }
+                else
+                {
+                    List<Ticket> sessionTickets = (List<Ticket>)Session["currentTickets"];
+                    sessionTickets.Add(ticket);
+                }               
+
+                return RedirectToAction("Index", "Ticket");
+                //return Json(new { redirectTo = Url.Action("Index", "Ticket") });
             }
 
-            return RedirectToAction("Index", "Ticket");
+            
+            // Post booking
+            return PartialView("BookEvent", booking);
         }
     }
 }
